ae7264252caad330e2e3fab9403a4c52baf11098
ebordenave <edward.bordenave@gmail.com>
2023-01-14 17:25:02 -0500

    leetcode

--- leetcode/02_static-arrays/26 remove_duplicates_from_sorted_array.py
+++ leetcode/02_static-arrays/26 remove_duplicates_from_sorted_array.py
@@ -1,12 +1,12 @@
 class Solution:
     def removeDuplicates(nums: list[int]) -> int:
         l = 1
-        
         for r in range(1, len(nums)):
             if nums[r] != nums[r-1]:
-                nums[l] = nums [r]
+                nums[l] = nums[r]
                 l += 1
         return l
+    #testing to see if remote tunneling works
     
 nums = [0,0,1,1,1,2,2,3,3,4]
 
--- leetcode/02_static-arrays/27 remove_element.py
+++ leetcode/02_static-arrays/27 remove_element.py
@@ -3,8 +3,12 @@
 
 
 def removeElement(self, nums: list[int], val: int) -> int:
-    l = 1
-    for r in range(1, len(nums)):
-        print(r)
+    k = 0
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[k] = nums[i]
+            k += 1
+    return k
             
 removeElement(5,nums=[8,9,2,3,5],val=1)
+
