239bdf672467435add8f4d8e9abc7e1280dc4f4b
Edward Bordenave <edward.bordenave@gmail.com>
2023-03-29 23:58:53 -0400

    49_group_anagrams

--- leetcode/49_group_anagrams.py
+++ leetcode/49_group_anagrams.py
@@ -1,28 +1,21 @@
-
-def groupAnagrams( strs: list[str]) -> list[list[str]]:
-    # create a dictionary     
+def groupAnagrams(strs: list[str]) -> list[list[str]]:
+    # Create an empty dictionary to store groups of anagrams
     dic={}
 
-    # for word in strings, create a var with the values of the sorted word, also put this word in a list
-    # if the sorted word is not in the dictionary, we add it dic[sortedword]=[word]
-    # else dic[sortedword] and we append word
-
+    # Iterate over each word in the input list of strings
     for word in strs:
-        #this creates a sorted
-        sortedword= "".join(sorted(list(word)))
+        # Sort the letters in the word alphabetically and join them back into a string
+        sortedword = "".join(sorted(list(word)))
 
-
+        # If the sorted word is not already a key in the dictionary, add it with the word as the value
         if sortedword not in dic:
             dic[sortedword]=[word]
-            # print(dic)
-
+        # Otherwise, append the word to the existing list of words associated with the sorted word
         else:
             dic[sortedword].append(word)
     
+    # Return a list of lists containing the values in the dictionary (i.e., the groups of anagrams)
     return print([i for i in dic.values()])
 
+# Call the groupAnagrams function with an example input
 groupAnagrams(strs = ["eat","tea","tan","ate","nat","bat"])
-
-
-
-
