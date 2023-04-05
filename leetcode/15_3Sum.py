66acdd4e358f993c9c9ec85156c5cf352a933045
Edward Bordenave <edward.bordenave@gmail.com>
2023-04-05 10:27:22 -0400

    leetcode

--- leetcode/15_3Sum.py
+++ leetcode/15_3Sum.py
@@ -1,24 +1,25 @@
 class Solution:
-    def ThreeSum(self, integers):
-        """
-        :type integers: List[int]
-        :rtype: List[List[int]]
-        """
+    def threeSum(self, integers: list[int]) -> list[list[int]]:
+        # sort the integers and create a list called result
         integers.sort()
         result = []
+        
+        # for loop iteration through the integers and if the value at index is greater than 0
         for index in range(len(integers)):
             if integers[index] > 0:
                 break
             if index > 0 and integers[index] == integers[index - 1]:
                 continue
             left, right = index + 1, len(integers) - 1
+            
+        # while left and rigth are not empty, if the int at left plus the int at right are less than 0 -- int at index, add left
             while left < right:
                 if integers[left] + integers[right] < 0 - integers[index]:
                     left += 1
                 elif integers[left] + integers[right] > 0 - integers[index]:
                     right -= 1
                 else:
-                    result.append([integers[index], integers[left], integers[right]]) # After a triplet is appended, we try our best to increase the numeric value of its first element or that of its second.
+                    result.append([integers[index], integers[left], integers[right]]) # After a triplet is appended, we try our best to incease the numeric value of its first element or that of its second.
                     left += 1 # The other pairs and the one we were just looking at are either duplicates or smaller than the target.
                     right -= 1 # The other pairs are either duplicates or greater than the target.
                     # We must move on if there is less than or equal to one integer in between the two integers.
--- leetcode/pivotIndex.py
+++ leetcode/pivotIndex.py
@@ -1,20 +1,13 @@
-def pivotIndex(self, nums):
-    leftSum, rightSum = 0, sum(nums)
-    # This loop iterates through each element in the nums list, 
-    # keeping track of the index of each element (idx) and the 
-    # value of each element (ele).
-    for idx, ele in enumerate(nums):
-        # The rightSum variable is updated by subtracting the 
-        # current element (ele) from it. This effectively 
-        # calculates the sum of all elements to the right of the current index.
-        rightSum -= ele
-        # The if statement checks if the sum of all 
-        # elements to the left of the current index 
-        # (leftSum) is equal to the sum of all elements 
-        # to the right of the current index (rightSum). 
-        # If they are equal, then the current index 
-        # is the pivot index, so the function returns idx.
-        if leftSum == rightSum:
-            return idx
-        leftSum += ele
-    return -1
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        leftSum, rightSum = 0, sum(nums)
+
+        for k,v in enumerate(nums):
+            rightSum -= v
+            if leftSum == rightSum:
+                return k
+            else:
+                leftSum += v
+        return -1
+            
+        
