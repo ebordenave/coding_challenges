f215a884a8c5bb73f733a02b20c86be6bef9436e
Edward Bordenave <edward.bordenave@gmail.com>
2023-04-14 23:52:15 -0400

    dsa

--- leetcode/XX_fib.py
+++ leetcode/XX_fib.py
@@ -1,32 +1,28 @@
 import time
 
+
 def fibonacci(n: int) -> int:
     # BASE CASE
     if n == 0:
         return 0
     if n == 1:
         return 1
-    
-    prev1, prev2 = 1,0
-    output = 0
-    
-    for idx in range(2,n+1):
-        output = prev1 + prev2
-        prev2 = prev1
-        prev1 = output
-    
-    # RECURSIVE CASE
-    # return fibonacci(n-1) + fibonacci(n-2)
-    
-    #(O)n
-    return output
+
+    fib_table = [0] * (n + 1)
+    fib_table[1] = 1
+
+    for idx in range(2, n+1):
+        fib_table[idx] = fib_table[idx - 1] + fib_table[idx - 2]
+
+    return fib_table
 
 
 def elapsed_time(func: callable, *args, **kwargs):
     start_time = time.time()
-    result = func(*args,**kwargs)
+    result = func(*args, **kwargs)
     end_time = time.time()
     elapsed_time = end_time - start_time
     return elapsed_time, result
 
-print(elapsed_time(fibonacci, 30))
+
+print(elapsed_time(fibonacci, 8))
